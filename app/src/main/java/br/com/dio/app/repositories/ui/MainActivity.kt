5a1692f96edc91fe613f4fc40b1087b24787d408
Roberto <robertovagner04@gmail.com>
2022-07-27 17:09:25 -0300

    adicionando menu

---
+++ app/src/main/java/br/com/dio/app/repositories/ui/MainActivity.kt
@@ -0,0 +1,47 @@
+package br.com.dio.app.repositories.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import br.com.dio.app.repositories.R
+import br.com.dio.app.repositories.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+
+        Log.e(TAG, "OnQueryTextSubmit: $query")
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+
+        Log.e(TAG, "OnQueryTextChange: $newText")
+        return false
+    }
+
+    companion object {
+        private const val TAG = "TAG"
+    }
+
+}
